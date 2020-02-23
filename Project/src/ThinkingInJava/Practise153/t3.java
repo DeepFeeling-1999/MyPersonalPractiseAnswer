5e6c5d60f6ac6119237a9ad1e73b9b7e8d815d8e
explorer1999 <2622282020@qq.com>
2020-02-23 23:01:39 +0800

    2020.2.23

---
+++ Project/src/ThinkingInJava/Practise153/t3.java
@@ -0,0 +1,15 @@
+package ThinkingInJava.Practise153;
+
+class temp1{
+    public void fun(){
+        System.out.println("temp1");
+    }
+}
+class temp2 extends temp1{
+
+}
+public class t3 {
+   public static void main(String []args){
+       new temp2().fun();
+   }
+}
