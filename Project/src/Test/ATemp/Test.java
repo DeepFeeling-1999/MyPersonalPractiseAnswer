d8d44d386192adbcbd7e5e66c596162a39b8f65e
explorer1999 <2622282020@qq.com>
2020-02-24 21:00:32 +0800

    666

--- Project/src/Test/ATemp/Test.java
+++ Project/src/Test/ATemp/Test.java
@@ -5,10 +5,7 @@ public class Test
     public static void main(String []args)
     {
 
-        System.out.println("");
+        System.out.println(Integer.toBinaryString(-2));
     }
 }
-class atemp{
-    private int num;
-    public int fun(){return num;}
-}
+
---
+++ Project/src/ThinkingInJava/Practise161/t12.java
@@ -0,0 +1,7 @@
+package ThinkingInJava.Practise161;
+
+public class t12 {
+    public static void main(String[]args){
+        
+    }
+}
