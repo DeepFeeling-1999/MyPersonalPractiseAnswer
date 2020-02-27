43f5f0472b4683074e69d1b64fb988bab004cc49
explorer1999 <2622282020@qq.com>
2020-02-27 22:38:19 +0800

    2020 2 27

--- Project/src/Test/ATemp/Test.java
+++ Project/src/Test/ATemp/Test.java
@@ -2,10 +2,21 @@ package Test.ATemp;
 
 public class Test
 {
-    public static void main(String []args)
-    {
-
-        System.out.println(Integer.toBinaryString(-2));
+    public static void main(String[] args) {
+        char today='日';
+        switch(today) {
+            case '一':
+            case '三':
+            case '五':
+                System.out.println("吃包子");
+                break;
+            case '二':
+            case '四':
+            case '六':
+                System.out.println("吃油条");
+                break;
+            default :
+                System.out.println("吃主席套餐");
+        }
     }
 }
-
--- Project/src/Test/ConstructorInnerPolymorphic/ConstructorInnerPolymorphic.java
+++ Project/src/Test/ConstructorInnerPolymorphic/ConstructorInnerPolymorphic.java
@@ -1,5 +1,9 @@
 package Test.ConstructorInnerPolymorphic;
 
+/**
+ * 简述：
+ * 构造器内部也有多态性，因此可能调用了未初始化的对象
+ */
 class superClass{
     int superNum=1;
     void draw(){
@@ -7,7 +11,7 @@ class superClass{
     }
     public superClass(){
         System.out.println("Super class constructor + superNum = "+superNum);
-        draw();
+        draw(); //在构造子类时，超类中动态绑定了子类方法，而此时子类尚未初始化
     }
 }
 class subClass extends superClass{
