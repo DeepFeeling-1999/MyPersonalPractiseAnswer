3c5a9bd00e90f3fc22bdd7fb81ecad6f411a2484
explorer1999 <2622282020@qq.com>
2020-03-15 08:12:32 +0800

    2020 3 15  8.12

--- Project/src/Test/ATemp/Test.java
+++ Project/src/Test/ATemp/Test.java
@@ -4,12 +4,18 @@ import java.util.Objects;
 
 public class Test
 {
-    public static void main(String[] args) {
-        int a=100;
-        int b=100;
-        System.out.println(Objects.hashCode(a)==Objects.hashCode(b));
+    public static void main(String[] args)
+    {
+        int a = 100;
+        int b = 100;
+        System.out.println(Objects.hashCode(a) == Objects.hashCode(b));
         System.out.println(System.identityHashCode(a));
         System.out.println(System.identityHashCode(b));
+
+    }
+
+    void fun()
+    {
 
     }
 }
--- Project/src/ThinkingInJava/Practise180/t13.java
+++ Project/src/ThinkingInJava/Practise180/t13.java
@@ -1,41 +1,57 @@
 package ThinkingInJava.Practise180;
 
-interface superInterface{
+interface superInterface
+{
     void superFun();
 }
-interface temp2 extends superInterface{
+
+interface temp2 extends superInterface
+{
     void temp2Fun();
 }
-interface temp3 extends superInterface{
+
+interface temp3 extends superInterface
+{
     void temp3Fun();
 }
 
-interface subInterface extends temp2,temp3{
+interface subInterface extends temp2, temp3
+{
     void subFun();
 }
 
-class testInterface implements subInterface{
-    public void superFun(){
+class testInterface implements subInterface
+{
+    public void superFun()
+    {
         System.out.println("testInterface superFun");
     }
 
-    public void temp2Fun(){
+    public void temp2Fun()
+    {
         System.out.println("testInterface temp2Fun");
     }
-    public void temp3Fun(){
+
+    public void temp3Fun()
+    {
         System.out.println("testInterface temp3Fun");
     }
-    public void subFun(){
+
+    public void subFun()
+    {
         System.out.println("testInterface subFun");
     }
 }
-public class t13 {
-    public static void main(String[]args){
-        testInterface t1=new testInterface();
 
-        ((superInterface)t1).superFun();
-        ((temp2)t1).temp2Fun();
-        ((temp3)t1).temp3Fun();
+public class t13
+{
+    public static void main(String[] args)
+    {
+        testInterface t1 = new testInterface();
+
+        ((superInterface) t1).superFun();
+        ((temp2) t1).temp2Fun();
+        ((temp3) t1).temp3Fun();
         t1.subFun();
 
         t1.superFun();
