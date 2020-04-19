8433d1757a3c43705216a846c3d29f6734fa7f83
explorer1999 <2622282020@qq.com>
2020-04-19 23:33:16 +0800

    2020 4 19 23.33

---
+++ Project/src/ThinkingInJava/Practise301_350/Practise309/t17.java
@@ -0,0 +1,19 @@
+package ThinkingInJava.Practise301_350.Practise309;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class t17
+{
+    public static void main(String[]args){
+        String st="//aaaa /* asdasda*/ \nccccccccccc cccccccc  //sadasdad \n/*asdasd\nadadadada\nadsad*/\n";
+        String myRegex="(//.+)|(/\\*.+)|(.+\\*/)";
+                          //"(//\\s.+)|(/\\*\\s+.+)|(\\*\\s+.+)"
+
+        Pattern p=Pattern.compile(myRegex);
+        Matcher m=p.matcher(st);
+
+        while(m.find())
+            System.out.println(m.group());
+    }
+}
