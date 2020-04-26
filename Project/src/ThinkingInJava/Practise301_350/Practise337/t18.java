241d7b3bcf61bbca49fe7a4f7afc202f32ca9d9e
explorer1999 <2622282020@qq.com>
2020-04-26 23:33:11 +0800

    2020 4 26 23.33

---
+++ Project/src/ThinkingInJava/Practise301_350/Practise337/t18.java
@@ -0,0 +1,28 @@
+package ThinkingInJava.Practise301_350.Practise337;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+
+public class t18 {
+    private static final Pattern p=Pattern.compile("(\\w+\\.)|\\s*final|\\s*native");
+
+    public static void main(String[]args){
+        try{
+            Class<?> c=Class.forName("ThinkingInJava.Practise301_350.Practise337.temp");
+            Method[]m=c.getDeclaredMethods();
+            Constructor<?>[]constructors=c.getDeclaredConstructors();
+            for(Method method:m){
+                System.out.println(p.matcher(method.toString()).replaceAll(""));
+            }
+            for(Constructor<?> constructor:constructors){
+                System.out.println(p.matcher(constructor.toString()).replaceAll(""));
+            }
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+            System.exit(666);
+        }
+    }
+}
---
+++ Project/src/ThinkingInJava/Practise301_350/Practise337/temp.java
@@ -0,0 +1,7 @@
+package ThinkingInJava.Practise301_350.Practise337;
+
+class temp {
+    public final void fun(int num) {
+        System.out.println(num);
+    }
+}
